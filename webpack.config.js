7a19ee5c4b3c62a3dd15cb48c136ea298bed39f5
shvetsovdm <sorrows@mail.ru>
2017-02-15 23:40:50 +1000

    Add webpack config and gutenberg typekit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-eval-source-map',
+    entry: ['./src/main.js', './src/main.sass'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'assets')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss|sass$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+                })
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                loader: 'file-loader'
+            },
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+    ]
+};
