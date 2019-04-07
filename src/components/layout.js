3c531699b2728f33aa7555af01fe8ffe1e7a14b3
shvetsovdm <shvetsovdm@gmail.com>
2019-04-07 16:51:26 +1000

    Adjust the desig to add blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
-import './layout.sass';
+import './base.sass';
+import './landing.sass';
+import './blog.sass';
 
 const Layout = ({ children }) => (
   <StaticQuery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout';
 
+import '../components/landing.sass';
+
 const IndexPage = () => (
   <Layout>
     <section className="sdm-layout__block">
-      <h1>I am <span className="sdm-accent">Dmitry</span></h1>
+      <h1 className="sdm-landing__title">I am <span className="sdm-accent">Dmitry</span></h1>
       <p>A <span className="sdm-accent">web developer</span> from Vladivostok, Russia.</p>
       <p>The <span className="sdm-accent">оrganaizer</span> of the <a className="sdm-multiline-link" href="https://ruby-vladivostok.github.io/">Ruby Vladivostok</a> meetup.</p>
       <p>You can find me on the internet as <span className="sdm-accent">@shvetsovdm</span>, writing articles and posting tweets about web development, Ruby, SQL, NoSQL, JavaScript, Node.js, programming in general, learning and self-development.</p>
       <p>For CV you can refer to <a className="sdm-multiline-link" href="https://www.linkedin.com/in/shvetsovdm/"><span className="sdm-accent">LinkedIn</span> profile</a>.</p>
     </section>
     <section className="sdm-layout__block">
-      <h2>My recent <span className="sdm-accent">articles</span></h2>
+      <h2 className="sdm-landing__title">My recent <span className="sdm-accent">articles</span></h2>
       <article className="sdm-article">
         <a className="sdm-multiline-link" href="https://medium.com/@shvetsovdm/99-bottles-of-object-oriented-programming-4c54480303e7">
           99 Bottles of Object Oriented Programming
@@ -40,7 +42,7 @@ const IndexPage = () => (
     </section>
     <div className="sdm-layout__block sdm-layout__block--multisection">
       <section className="sdm-layout__section">
-        <h2>Find me in <span className="sdm-accent">social networks</span></h2>
+        <h2 className="sdm-landing__title">Find me in <span className="sdm-accent">social networks</span></h2>
         <ul>
           <li><a rel="external" href="https://github.com/shvetsovdm">Github</a></li>
           <li><a rel="external" href="https://stackoverflow.com/users/6317812/shvetsovdm">StackOverflow</a></li>
@@ -50,7 +52,7 @@ const IndexPage = () => (
         </ul>
       </section>
       <section className="sdm-layout__section">
-        <h2>Goodies:</h2>
+        <h2 className="sdm-landing__title">Goodies</h2>
         <ul>
           <li><a className="sdm-multiline-link" rel="external" href="https://www.youtube.com/playlist?list=PLZ9y9yxqfSW-HUoh2r-WCWU8ewrBI_8nm">Collection of great videos for programmers</a></li>
         </ul>
