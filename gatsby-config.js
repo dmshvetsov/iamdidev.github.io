09d5181973a47e84ca4d662c14314e6339e37ff5
Di <helloiamdi@gmail.com>
2019-06-17 20:02:15 +1000

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,6 +88,12 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-47078794-2',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
